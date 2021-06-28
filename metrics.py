b6a4d6be090dfa7bcd948db2ad595dbd913c59d8
Patrick Joyce <pjaudiomv@gmail.com>
2021-06-28 01:14:36 -0400

    error messaging

--- metrics.py
+++ metrics.py
@@ -6,15 +6,18 @@ import boto3
 from boto3.dynamodb.conditions import Key
 
 table = boto3.resource('dynamodb').Table(os.environ['DYNAMO_TABLE'])
+current_date = datetime.today().strftime('%Y-%m-%d')
 
-bad_response = {
-    "statusCode": 403,
-    "headers": {"content-type": "application/json"},
-    "body": json.dumps({ "response" : "bad" })
-}
+
+def bad_response(message):
+    return {
+        "statusCode": 403,
+        "headers": {"content-type": "application/json"},
+        "body": json.dumps({"error_response": message})
+    }
+
 
 def logger_handler(event, context):
-    current_date = datetime.today().strftime('%Y-%m-%d')
     num_zones = num_regions = num_areas = num_meetings = num_groups = 0
     req = urllib3.PoolManager().request("GET", 'https://tomato.bmltenabled.org/rest/v1/rootservers/')
     root_servers = json.loads(req.data.decode())
@@ -40,8 +43,7 @@ def api_handler(event, context):
     print(event)
     path = event.get("path")
     if not path.startswith("/metrics"):
-        print("bad path")
-        return bad_response
+        return bad_response("bad path")
     fe = Key('date').between('2021-06-27', current_date)
     response = table.scan(FilterExpression=fe)
 
